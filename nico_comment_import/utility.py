ad24b58d86c49bd67ce142d229aca68788ac91fe
danicofan <hi_king_ko_1@hotmail.com>
2017-12-11 01:16:09 +0900

    :art: retry

---
+++ nico_comment_import/utility.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import random
+
+
+def retry_call(f, random_time, max_retry=3):
+    for _ in range(max_retry):
+        try:
+            return f()
+        except:
+            random.randint(random_time)
+    return f()
